461e2f1416c5ec389b4b867626fd7d38c171287e
OksanaZhmurko <oksanazhmurko@gmail.com>
2023-12-06 00:30:16 +0200

    Update cat-api.js

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,6 +1,6 @@
 const url = 'https://api.thecatapi.com/v1';
 axios.defaults.headers.common ["x-api-key"] = "live_KKRXbc8Dc9yTNb06QWF9f9Y2hWjxGTgmp9FluGVsZzhFdQncv0u4kbJ7pBLqymZ9";
-
+axios.defaults.headers.common ["x-api-key"] = "live_KKRXbc8Dc9yTNb06QWF9f9Y2hWjxGTgmp9FluGVsZzhFdQncv0u4kbJ7pBLqymZ9";
 export function fetchBreeds() {
     return fetch(`${url}/breeds?api_key=${api_key}`)
         .then(response => {
@@ -10,8 +10,6 @@ export function fetchBreeds() {
             return response.json();
         });       
 };
-
-//https://api.thecatapi.com/v1/images/search
 
 export function fetchCatByBreed(breedId){
     return fetch(`${url}/images/search?api_key=${api_key}&breed_ids=${breedId}`)
